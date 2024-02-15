a356e576e9c6e91c3303b55c5772371572a7f324
unknown <hammad@sourcesketch.com>
2024-02-16 00:06:17 +0500

    working on home page

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
     // click on menu button
     $("#menu").on("click", function(){
-      $(".dp-header-main__box").slideToggle("slow");
-      $("#cards").slideToggle("slow");
+      $(".dp-header-main__box").slideDown().toggleClass("hidden");
+      $("#cards").slideToggle("");
       $(".dp-header-main__text").toggleClass("text-white");
-      
+      $(".dp-header-main__menu").toggleClass("bg-white");
+      $("#menuIcon").toggleClass("hidden");
+      $("#closeIcon").toggleClass("hidden"); 
     });
+    // $('.path').slideToggle("slow").animate({scaleX: '1660', scaleY});
 });
