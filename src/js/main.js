803ffebf27bfb43737c8c924635af5a7f18c0bfb
unknown <hammad@sourcesketch.com>
2024-02-26 01:12:47 +0500

    added sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,28 @@
-gsap.fromTo('.dp-top-img',{transform: 'scaleX(0.9)', opacity: '0.1'}, {transform: 'scale(1)',
+gsap.to('.dp-top-img svg', { 
+
     scrollTrigger: {
         trigger: '.dp-top-img',
-        start: 'top center-=70',
+        scrub: 1,
+        start: 'top center-=100',
         end: "bottom bottom",
+        rotation: 90,
         markers: true,
-        toggleActions: 'play none reverse none'
+        toggleActions: 'play none reverse none',
+       duration: 0.5,
+        stagger: 0.075,
     }
-});
-gsap.to('.dp-intro__logo', { x: '-100vw',
+}, 0);
+gsap.to('.dp-intro__logo', { x: '100vw',
     scrollTrigger: {
         trigger: '.dp-intro__logo',
         start: 'top center-=10',
         end: "bottom bottom",
         markers: true,
-        toggleActions: 'play none reverse none'
+        toggleActions: 'play none reverse none',
+        duration: 0.5
     }
-});
-gsap.to('.dp-intro__content', { x: '100vw',
+}, 0);
+gsap.to('.dp-intro__content', { x: '-100vw',
     scrollTrigger: {
         trigger: '.dp-intro__content',
         start: 'top center-=1',
