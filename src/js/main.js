05e4db21cb437088c8abcc2a8c5ae06531736022
qain@ad.mcldev.co <qain@ad.mcldev.co>
2024-02-27 07:04:24 -0800

    working on gsap animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,33 @@
-gsap.to('.dp-top-img svg', { 
 
+
+
+gsap.to(
+  ".dp-intro__logo",
+  {
+    x: "100vw",
     scrollTrigger: {
-        trigger: '.dp-top-img',
-        scrub: 1,
-        start: 'top center-=100',
-        end: "bottom bottom",
-        rotation: 90,
-        markers: true,
-        toggleActions: 'play none reverse none',
-       duration: 0.5,
-        stagger: 0.075,
-    }
-}, 0);
-gsap.to('.dp-intro__logo', { x: '100vw',
-    scrollTrigger: {
-        trigger: '.dp-intro__logo',
-        start: 'top center-=10',
-        end: "bottom bottom",
-        markers: true,
-        toggleActions: 'play none reverse none',
-        duration: 0.5
-    }
-}, 0);
-gsap.to('.dp-intro__content', { x: '-100vw',
-    scrollTrigger: {
-        trigger: '.dp-intro__content',
-        start: 'top center-=1',
-        end: "bottom bottom",
-        markers: true,
-        toggleActions: 'play none reverse none',
-        
-    }
+      trigger: ".dp-intro__logo",
+      start: "top-=10",
+      end: "bottom bottom",
+      markers: true,
+      toggleActions: "play none reverse none",
+      duration: 0.5,
+      stagger: 0.05,
+    },
+  });
+gsap.to(".dp-intro__content", {
+  x: "-100vw",
+  scrollTrigger: {
+    trigger: ".dp-intro__content",
+    start: "top center-=1",
+    end: "bottom bottom",
+    markers: true,
+    toggleActions: "play none reverse none",
+    stagger: 0.05,
+  },
+});
+
+gsap.to(section, {
+  opacity: 1,
+  stagger: 0.2,
 });
